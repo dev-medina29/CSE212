98771e4cbae17f6beba0794307841089cb1ee8df
dev-medina29 <mmbedi@byupathway.edu>
2026-05-17 15:44:09 +0100

    test2

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -30,8 +30,17 @@ public static class Arrays
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+
+        // 1. Create  the left part of the list
+        List<int> leftPart=data.GetRange(0,data.Count-amount);
+
+        // 2. Create the right part of the list
+        List<int> rightPart=data.GetRange(data.Count-amount,amount);
+
+        // 3. Create the rotated list
+        List<int> rotatedList = new List<int>(rightPart);
+        rotatedList.AddRange(leftPart);
+        data.Clear();
+        data.AddRange(rotatedList);
     }
 }
