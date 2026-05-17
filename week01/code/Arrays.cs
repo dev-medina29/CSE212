3eec1f1c104007efbe1531b51001cf835325b3a6
dev-medina29 <mmbedi@byupathway.edu>
2026-05-17 14:32:51 +0100

    first_test

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -9,11 +9,15 @@ public static class Arrays
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // 1. Create an array of doubles with the size of 'length'
+        double[] multiples = new double[length];
 
-        return []; // replace this return statement with your own
+        //2. list of multiples of number, starting with number and ending with number * length
+        for (int i=1; i<=length; i++)
+        {
+            multiples[i - 1] = number * i;
+        }
+        return multiples; 
     }
 
     /// <summary>
